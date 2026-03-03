0de81d43fb7d5b4a92312258a3ca7ee60d8ad5fc
Shunmuga Sundaram P <shunmugasundaram003@gmail.com>
2026-03-03 16:03:53 +0530

    Update DecodeStrings.java

--- DecodeStrings.java
+++ DecodeStrings.java
@@ -1,27 +1,29 @@
 package ZohoTharuvai.Biweekly3;
 
+import java.util.Arrays;
+
 public class DecodeStrings {
     public static void main(String[] args) {
-        String s="12";
-        System.out.println(decode(0,s));
+        String s = "112";
+        Integer[] dp = new Integer[s.length()];
+        System.out.println(decode(0, s, dp));
+        System.out.println(Arrays.toString(dp));
     }
-    public static int decode(int index,String s){
-        Integer[] dp=new Integer[s.length()];
-        return helper(0, s,dp);
+    public static int decode(int index, String s, Integer[] dp) {
+        return helper(index, s, dp);
     }
 
-    private static int helper(int index, String s,Integer[] dp) {
+    private static int helper(int index, String s, Integer[] dp) {
         if (index == s.length()) return 1;
         if (s.charAt(index) == '0') return 0;
-        if(dp[index]!=null) return dp[index];
+        if (dp[index] != null) return dp[index];
         int count = 0;
-        count += helper(index + 1, s,dp);
-        if (index + 1 < s.length()) {
-            int twoDigit = Integer.parseInt(s.substring(index, index + 2));
-            if (twoDigit >= 10 && twoDigit <= 26) {
-                count += helper(index + 2, s,dp);
-            }
+        count += helper(index + 1, s, dp);
+        if (index + 1 < s.length() &&
+                (s.charAt(index) == '1' ||
+                        (s.charAt(index) == '2' && s.charAt(index + 1) <= '6'))) {
+            count += helper(index + 2, s, dp);
         }
-        return dp[index]=count;
+        return dp[index] = count;
     }
 }
